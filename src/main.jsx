8c5ed1b344d93c4202e96a7b95427eb0d02d21c2
suhanigupta25 <suhani259gupta@gmail.com>
2026-08-30 18:00:49 +0530

    adding pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./pwa";
 import App from "./App.jsx";
+import { syncManager } from "./sync/syncManager"; // side-effect import kicks off its constructor
+
+syncManager.trigger(); // attempt a sync immediately on app load, in case items were queued last session
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
